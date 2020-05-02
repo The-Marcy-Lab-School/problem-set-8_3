f1ac2fe1f7af38bf09102b4954fbe261937cda0f
Reuben Ogbonna <reubenogbonna@gmail.com>
2020-05-01 20:24:35 -0400

    updates with useEffect

--- app/src/Board.jsx
+++ app/src/Board.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import BoardContext from './contexts/BoardContext';
 import Square from './Square';
 
@@ -20,9 +20,9 @@ const Board = () => {
     ];
 
     for (const combo of winningCombos) {
-      const marks = combo.map((position) => board[position])
-      if (marks.every((mark) => mark === 'X') ||
-          marks.every((mark) => mark === 'O')) {
+      const marks = combo.map((position) => board[position]);
+      if (marks.every((playerMark) => playerMark === 'X')
+          || marks.every((playerMark) => playerMark === 'O')) {
         return marks[0];
       }
     }
@@ -30,30 +30,40 @@ const Board = () => {
     return null;
   };
 
+  useEffect(() => {
+    setWinner(() => getWinner());
+  }, [board]);
+
   const handleClick = (squareNumber) => {
+    if (winner) {
+      return;
+    }
+
     setBoard((oldBoard) => {
       const newBoard = [...oldBoard];
       newBoard[squareNumber] = mark;
       return newBoard;
     });
 
-    setMark((oldMark) => oldMark === 'X' ? 'O' : 'X');
-
-    setWinner(() => getWinner());
-
-    console.log({ mark, board, winner });
+    setMark((oldMark) => (oldMark === 'X' ? 'O' : 'X'));
   };
-  
+
   return (
-    <BoardContext.Provider value={{board, handleClick}}>
+    <BoardContext.Provider value={{ board, handleClick }}>
       <div>
-        { winner ? <h1>{winner} Wins!</h1> : ''}
+        { winner ? (
+          <h1>
+            {winner}
+            {' '}
+            Wins!
+          </h1>
+        ) : ''}
         <div className="board">
           { board.map((_, index) => <Square id={index} />) }
         </div>
       </div>
     </BoardContext.Provider>
   );
-}
+};
 
 export default Board;
